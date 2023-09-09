1d924450a5f2c1c7fc5ec9fa597c1bbc4d51b0fd
martin wangondu <wangondumn@gmail.com>
2023-09-09 19:18:49 +0300

    Added About page

--- pages/about/index.js
+++ pages/about/index.js
@@ -18,28 +18,13 @@ import {
   SiNextdotjs,
 } from 'react-icons/si';
 
+import CountUp from 'react-countup';
+
 // data
 export const aboutData = [
   {
     title: 'skills',
-    // info: [
-    //   {
-    //     title: 'Web Development',
-    //     icons: [
-    //       <FaHtml5 />,
-    //       <FaCss3 />,
-    //       <FaJs />,
-    //       <FaReact />,
-    //       <SiNextdotjs />,
-    //       <SiFramer />,
-    //       <FaWordpress />,
-    //     ],
-    //   },
-    //   {
-    //     title: 'UI/UX Design',
-    //     icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-    //   },
-    // ],
+
     info: [
       {
         title: 'Web Development',
@@ -50,11 +35,10 @@ export const aboutData = [
           <FaReact key="react" />,
           <SiNextdotjs key="nextjs" />,
           <SiFramer key="framer" />,
-          <FaWordpress key="wordpress" />,
         ],
       },
       {
-        title: 'UI/UX Design',
+        title: 'Data Analysis/Science',
         icons: [
           <FaFigma key="figma" />,
           <SiAdobexd key="adobexd" />,
@@ -63,19 +47,19 @@ export const aboutData = [
       },
     ],
   },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
+  // {
+  //   title: 'awards',
+  //   info: [
+  //     {
+  //       title: 'Webby Awards - Honoree',
+  //       stage: '2011 - 2012',
+  //     },
+  //     {
+  //       title: 'Adobe Design Achievement Awards - Finalist',
+  //       stage: '2009 - 2010',
+  //     },
+  //   ],
+  // },
   {
     title: 'experience',
     info: [
@@ -127,24 +111,94 @@ const About = () => {
         exit="hidden"
         className="hidden xl:flex absolute bottom-0 -left-[370px]"
       >
-        <Avatar />
+        {/* <Avatar /> */}
       </motion.div>
 
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
         <div className="flex-1 flex flex-col justify-center">
-          <h2 className="h2">
-            Captivating<span>stories</span> birth magnificent design.
-          </h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+          <motion.h2
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Captivating<span className="text-indigo-500"> stories</span>{' '}
+            birth magnificent design.
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
             10 years ago, I bagan freelancing as a developer. Since then, I,ve
             done remote work for agencies, counsulted for startups, and
             collaborated on digital products for business and consumer use.
-          </p>
+          </motion.p>
+
+          {/* counters */}
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
+            <div className="flex flex-1 xl:gap-x-6">
+              {/* experience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
+                  <CountUp start={0} end={2} duration={8} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]:">
+                  Years of experience
+                </div>
+              </div>
+
+              {/* customers */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]:">
+                  Satisfied customers
+                </div>
+              </div>
+
+              {/* Projects */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
+                  <CountUp start={0} end={15} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]:">
+                  Finished Projects
+                </div>
+              </div>
+
+              {/* awards */}
+              <div className="relative flex-1">
+                <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
+                  <CountUp start={0} end={5} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]:">
+                  Winning Awards
+                </div>
+              </div>
+            </div>
+          </motion.div>
         </div>
 
         {/* info */}
-        <div className="flex flex-col w-full xl:max-w-[48%] h-[400px]">
+        <motion.div
+          variants={fadeIn('left', 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[400px]"
+        >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((data, dataIndex) => {
               return (
@@ -153,7 +207,7 @@ const About = () => {
                   className={`${
                     index === dataIndex &&
                     'text-indigo-500 after:w-[100%] after:text-indigo-500 after:transition-all after:duration-300 '
-                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-indigo-500/75 after:absolute after:-bottom-1 after:left-0`}
                   onClick={() => setIndex(dataIndex)}
                 >
                   {data.title}
@@ -184,7 +238,7 @@ const About = () => {
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,39 @@
-import Image from "next/image"
-import { motion } from "framer-motion";
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
-import ParticlesContainer from "../components/ParticlesContainer";
-import ProjectsBtn from "../components/ProjectsBtn";
-import Avatar from "../components/Avatar";
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
 
-import { fadeIn } from '../variants'
+import { fadeIn } from '../variants';
 
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
-
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/90 to-black/20">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-
           {/* tittle */}
-          <motion.h1 variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" className="h1">Transfoming Ideas
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Transfoming Ideas
             <br />
-            <span className="text-accent">Digital Reality</span>
+            <span className="text-indigo-500/75">Digital Reality</span>
           </motion.h1>
-          <motion.p variants={fadeIn('down', 0.3)} initial="hidden" animate="show" exit="hidden" className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In modi, facilis molestiae reprehenderit quas officiis ullam sunt veniam sit quo nihil doloribus unde voluptatibus eos est architecto. Veritatis, quod modi?
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In modi,
+            facilis molestiae reprehenderit quas officiis ullam sunt veniam 
           </motion.p>
 
           {/* button */}
@@ -29,19 +41,25 @@ const Home = () => {
             <ProjectsBtn />
           </div>
 
-          <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show" exit="hidden"
-            className="hidden xl:flex">
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
             <ProjectsBtn />
           </motion.div>
         </div>
       </div>
       {/* image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
-
-        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-hue translate-z-0'>image</div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-hue translate-z-0">
+          image
+        </div>
 
         {/* particles */}
-        <ParticlesContainer className='w-full h-full' />
+        <ParticlesContainer className="w-full h-full" />
 
         {/* my avatar */}
         <motion.div
@@ -51,12 +69,13 @@ const Home = () => {
           exit="hidden"
           transition={{ duration: 1, ease: 'easeInOut' }}
           className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-40 -right-0 lg:-bottom-55
-         lg:right-[8%]">
+         lg:right-[8%]"
+        >
           <Avatar />
         </motion.div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
