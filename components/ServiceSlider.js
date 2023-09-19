f6e2737458c4a975ad8cbbbaac550e02a7e08e11
mine-martin <wangondumn@gmail.com>
2023-09-19 18:48:43 +0300

    Added Services Page

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,4 +1,24 @@
-import { RxCrop, RxDesktop, RxPencil2, RxReader, RxRocket } from "react-icons/rx";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import { FreeMode, Pagination } from 'swiper';
+
+// icons
+import {
+  RxArrowTopLeft,
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+} from 'react-icons/rx';
+
+// framermotion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 // data
 const serviceData = [
@@ -30,7 +50,52 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spacesBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spacesBetween: 15,
+        },
+      }}
+      FreeMode ={true}
+      pagination={{
+        clickable: true
+      }}
+      className='h-[240px] sm:h-[340px]'
+    >
+      {
+        serviceData.map((items, index) =>{
+          return (
+            <SwiperSlide key={index}>
+              <div className='bg-[rgba(65,47,132,0.15)] h-max rounded-lg px-6 py-8
+              flex sm:flex-col gap-x-6'>
+                {/* icons */}
+                <div>ivons</div>
+
+                {/* title and description */}
+
+                <div>
+                  <div>{items.title}</div>
+                  <p>{items.description}</p>
+                </div>
+                <div>
+                  {/* arrow */}
+                  <div>
+                    <RxArrowTopLeft />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -2,16 +2,28 @@ import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles';
 import ServiceSlider from '../../components/ServiceSlider';
 
-//icons
-import {} from 'react-icons/rx';
-
-import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants';
-
 const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-6 flex items-center">
       <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb0">
+            <h2 className="h2 xl:mt-8 ">
+              My Services
+              <span className="text-indigo-500">.</span>
+            </h2>
+            <p className="mb-4 max-w[400px] mx-auto lg:mx-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+
+          {/* slider */}
+          <ServiceSlider />
+        </div>
+      </div>
       <Bulb />
     </div>
   );
