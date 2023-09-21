f3d357f052921993b5fc4af676fbb263f753fa5b
mine-martin <wangondumn@gmail.com>
2023-09-21 18:38:28 +0300

    Finishing up with Services Page

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,6 +2,10 @@ import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles';
 import ServiceSlider from '../../components/ServiceSlider';
 
+// framermotion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
@@ -10,19 +14,38 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8 ">
+            <motion.h2
+              variants={fadeIn('down', 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8 "
+            >
               My Services
               <span className="text-indigo-500">.</span>
-            </h2>
-            <p className="mb-4 max-w[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w[400px] mx-auto lg:mx-0"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
               ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+            </motion.p>
           </div>
-          <div className='w-full xl:max-w-[65%] '>
-            {/* slider */}
+
+          {/* slider */}
+          <motion.div
+            variants={fadeIn('left', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%] "
+          >
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       <Bulb />
