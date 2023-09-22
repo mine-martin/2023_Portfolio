7d5c8e092dc0e659dc6ac73e2d9d8d1d6d4f2d50
mine-martin <wangondumn@gmail.com>
2023-09-22 20:52:15 +0300

    Updates

--- components/Nav.js
+++ components/Nav.js
@@ -27,19 +27,6 @@ const links = [
   },
 ];
 
-export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about ', icon: <HiUser /> },
-  { name: 'services', path: '/services ', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work ', icon: <HiViewColumns /> },
-  // {
-  //   name: 'testimonials',
-  //   path: '/testimonials',
-  //   icon: <HiChatBubbleBottomCenterText />,
-  // },
-  { name: 'contact', path: '/contact', icon: <HiEnvelope /> },
-];
-
 const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
